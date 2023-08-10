711b793a584129af2c1bf35411379b9132df772b
Yogesh Thakur <ythakur@onetrust.com>
2023-08-10 15:20:47 +0530

    edpisode-02

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'Hi i am H1 tag'), React.createElement('h2', {}, 'Hi i am H2 tag')]));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'Hi Namsaste React'), React.createElement('h2', {}, 'Hi i am H2 tag')]));
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- Hot Module Replacement
+- File Watching Algorithm
+- Caching - Faster Builds
+- Image Optimization
+- Minification of files
+- Bundling
+- Compressing
+- Consitent Hashing
+- Code Splitting
+- Differential Bundling
+- Diagnostic
+- Error Handling
+- Tree Shaking
+- Different dev and prod build
+
