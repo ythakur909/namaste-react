2784fb3bef6a59a4c438af5ce2b307514146cad3
Yogesh Thakur <ythakur@onetrust.com>
2023-08-16 20:14:32 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-const parent = React.createElement('div', { id: 'parent' }, React.createElement('div', { id: 'child' }, [React.createElement('h1', {}, 'Hi Namsaste React'), React.createElement('h2', {}, 'Hi i am H2 tag')]));
-console.log(parent);
+
+// const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React');
+
+//JSX transpiled before it reaches the js (parcel-babel)
+//JSX => React.createElement => ReactElement-JS Object => HTMLELement(Render)
+const Title = () => (<h1 id='heading' className='head' tabIndex='5'>
+    Namaste Javscript using jsx
+</h1>);
+const count = 1000;
+const Heading = () => <div id="container">
+    {Title()}
+    <Title />
+    <Title></Title>
+    {count}
+    <h1 className='heading'>Namaste Javascript using functional component</h1>
+</div>
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+
+root.render(<Heading />);
