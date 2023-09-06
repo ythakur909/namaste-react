d67ec71c105f23caf749455a5d1c11be8b582f30
Yogesh Thakur <ythakur@onetrust.com>
2023-09-06 17:00:44 +0530

    episode-04

--- App.js
+++ App.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React');
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-//JSX transpiled before it reaches the js (parcel-babel)
-//JSX => React.createElement => ReactElement-JS Object => HTMLELement(Render)
-const Title = () => (<h1 id='heading' className='head' tabIndex='5'>
-    Namaste Javscript using jsx
-</h1>);
-const count = 1000;
-const Heading = () => <div id="container">
-    {Title()}
-    <Title />
-    <Title></Title>
-    {count}
-    <h1 className='heading'>Namaste Javascript using functional component</h1>
-</div>
+const Card = () => {
+    return (
+        <div className="res-card">
+            <img className='res-logo' src="https://b.zmtcdn.com/data/pictures/1/50691/92d9b4053ef0965120828b4fa4eecc3b.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*" />
+            <h3>Meghana Briyani</h3>
+            <h4>Briyani, North-Indian, Asian</h4>
+            <h4>4.4 Star</h4>
+            <h4>26min</h4>
+        </div>
+    )
+}
 
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>Search</div>
+            <div className='res-container'>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<Heading />);
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout />);
